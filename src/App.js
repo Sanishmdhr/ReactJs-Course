b62588d5c68fb4357d66feaebccc54bce8795535
Sanishmdhr <sanish.manandhar20@gmail.com>
2020-09-01 12:25:51 +0545

    Configure React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {Navbar, NavbarBrand} from 'reactstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* 
+      1. install bootstrap, reactstrap and react-popper 
+      2. we will link bootstrap in our project 
+      3. create a basic navbar
+    
+     */}
+
+     <Navbar color="warning" dark expand='md'>
+        <div className='container'>
+            <NavbarBrand href='/'>
+              Food Fusion
+            </NavbarBrand>
+        </div>
+     </Navbar>
+
+
     </div>
   );
 }
