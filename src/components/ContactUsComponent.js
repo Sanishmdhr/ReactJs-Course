06fb903b0d4b901be9a56f9f6e2eae5cf1434bf6
Sanishmdhr <sanish.manandhar20@gmail.com>
2020-09-08 10:41:07 +0545

    Controlled Forms Validation

--- src/components/ContactUsComponent.js
+++ src/components/ContactUsComponent.js
@@ -1,40 +1,82 @@
 import React, { Component } from 'react';
 import '../css/Contactus.css';
-import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Input, Col, Button } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Input, Col, Button, FormFeedback } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 class ContactUsComponent extends Component {
 
   constructor(props) {
     super(props)
-  
+
     this.state = {
-       firstname:'',
-       lastname:'',
-       telnum:'',
-       email:'',
-       agree:false,
-       contactType:'Tel.',
-       message:''
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+      agree: false,
+      contactType: 'Tel.',
+      message: '',
+      touched: {
+        firstname: false,
+        lastname: false,
+        telnum: false,
+        email: false
+      }
     }
 
     this.handleInput = this.handleInput.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
-
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
-  handleInput(event){
+  handleInput(event) {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
-      this.setState({
-        [name]:value
-      })     
+    this.setState({
+      [name]: value
+    })
   }
-  
-  onFormSubmit(event){
+
+  handleBlur = (field) => (event) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true }
+    })
+  }
+
+  validate(firstname, lastname, telnum, email) {
+    const error = {
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: ''
+    };
+
+    if (this.state.touched.firstname && firstname.length < 3)
+      error.firstname = "First Name Should be >= 3 characters";
+    else if (this.state.touched.firstname && firstname.length > 10)
+      error.firstname = "First Name Should be <=10 characters";
+
+
+    if (this.state.touched.lastname && lastname.length < 3)
+      error.lastname = "Last Name Should be >= 3 characters";
+    else if (this.state.touched.lastname && lastname.length > 10)
+      error.lastname = "Last Name Should be <=10 characters";
+
+    const reg = /^\d+$/;
+    if (this.state.touched.telnum && !reg.test(telnum))
+      error.telnum = "Tel. number should contain only numbers";
+
+
+    if (this.state.touched.email && email.split('').filter(x => x === "@").length !== 1)
+      error.email = 'Email should contain a @';
+
+    return error;
+  }
+
+  onFormSubmit(event) {
     event.preventDefault();
-      alert(`
+    alert(`
       firstname:${this.state.firstname},
       lastname:${this.state.lastname},
       telnum:${this.state.telnum},
@@ -46,6 +88,8 @@ class ContactUsComponent extends Component {
   }
 
   render() {
+    const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
+
     return (
       <div className="container">
         <div className='row'>
@@ -100,10 +144,15 @@ class ContactUsComponent extends Component {
                     type='text'
                     id="firstname"
                     name='firstname'
+                    valid={errors.firstname === ''}
+                    invalid={errors.firstname !== ''}
                     placeholder="Enter Your First Name"
+                    onBlur={this.handleBlur('firstname')}
                     value={this.state.firstname}
                     onChange={this.handleInput}
                   />
+                  <FormFeedback>{errors.firstname}</FormFeedback>
+
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -113,10 +162,14 @@ class ContactUsComponent extends Component {
                     type='text'
                     id="lastname"
                     name='lastname'
+                    valid={errors.lastname === ''}
+                    invalid={errors.lastname !== ''}
                     placeholder="Enter Your Last Name"
+                    onBlur={this.handleBlur('lastname')}
                     value={this.state.lastname}
                     onChange={this.handleInput}
                   />
+                  <FormFeedback>{errors.lastname}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -126,10 +179,15 @@ class ContactUsComponent extends Component {
                     type='tel'
                     id="telnum"
                     name='telnum'
+                    valid={errors.telnum === ''}
+                    invalid={errors.telnum !== ''}
                     placeholder="Enter Your Contact Number"
+                    onBlur={this.handleBlur('telnum')}
                     value={this.state.telnum}
                     onChange={this.handleInput}
                   />
+                  <FormFeedback>{errors.telnum}</FormFeedback>
+
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -139,10 +197,15 @@ class ContactUsComponent extends Component {
                     type='email'
                     id="email"
                     name='email'
+                    valid={errors.email === ''}
+                    invalid={errors.email !== ''}
                     placeholder="Enter Your Email Address"
+                    onBlur={this.handleBlur('email')}
                     value={this.state.email}
                     onChange={this.handleInput}
                   />
+                  <FormFeedback>{errors.email}</FormFeedback>
+
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -186,9 +249,9 @@ class ContactUsComponent extends Component {
 
               </FormGroup>
               <FormGroup row>
-                <Col md={{size:10, offset:2}}>
-                    <Button type='submit' color='primary'>
-                      Send Feedback
+                <Col md={{ size: 10, offset: 2 }}>
+                  <Button type='submit' color='primary'>
+                    Send Feedback
                     </Button>
                 </Col>
               </FormGroup>
