c26a72140b146b3d3772969c0fa8cf02ceaf474a
Sanishmdhr <sanish.manandhar20@gmail.com>
2020-09-14 12:01:59 +0545

    Fetch Handling Errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,5 @@
 import * as ActionTypes from './ActionTypes';
-import {DISHES} from '../shared/dishes'; 
+import { DISHES } from '../shared/dishes';
 import { baseUrl } from '../shared/baseUrl';
 
 
@@ -11,53 +11,82 @@ export const fetchDishes = () => (dispatch) => {
 
   dispatch(dishLoading(true));
 
-  return fetch(baseUrl+'dishes')
-  .then(response => response.json())
-  .then(dishes =>  dispatch(addDishes(dishes)))
-   
+  return fetch(baseUrl + 'dishes')
+    .then(response => {
+      if (response.ok) {
+        return response;
+      }
+      else {
+        var error = new Error('Error' + response.status + ":" + response.statusText)
+        error.response = response;
+        throw error;
+      }
+    },
+      error => {
+        var errmess = new Error(error.message)
+        throw errmess;
+      })
+    .then(response => response.json())
+    .then(dishes => dispatch(addDishes(dishes)))
+    .catch(error => dispatch(dishesFailed(error.message)))
 
 }
 
 
 export const dishLoading = () => ({
-  type:ActionTypes.DISHES_LOADING
+  type: ActionTypes.DISHES_LOADING
 })
 
 export const dishesFailed = (errmess) => ({
-  type:ActionTypes.DIHSES_FAILED,
-  payload:errmess  
+  type: ActionTypes.DIHSES_FAILED,
+  payload: errmess
 })
 
 
 export const addDishes = (dishes) => ({
-  type:ActionTypes.ADD_DISHES,
-  payload:dishes  
+  type: ActionTypes.ADD_DISHES,
+  payload: dishes
 })
 
 
 //to fetch the comments 
 
 export const fetchComments = () => (dispatch) => {
-  return fetch(baseUrl+'comments')
-  .then(response=>response.json())
-  .then(comments => dispatch(addComments(comments)))
+  return fetch(baseUrl + 'comments')
+    .then(response => {
+      if (response.ok) {
+        return response;
+      }
+      else {
+        var error = new Error('Error' + response.status + ":" + response.statusText)
+        error.response = response;
+        throw error;
+      }
+    },
+      error => {
+        var errmess = new Error(error.message)
+        throw errmess;
+      })
+    .then(response => response.json())
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)))
 
 }
 
 
 export const addComments = (comments) => ({
-  type:ActionTypes.ADD_COMMENTS,
-  payload:comments
+  type: ActionTypes.ADD_COMMENTS,
+  payload: comments
 })
 
 export const commentsFailed = (errmess) => ({
-  type:ActionTypes.COMMENTS_FAILED,
-  payload:errmess
+  type: ActionTypes.COMMENTS_FAILED,
+  payload: errmess
 })
 
-export const addComment = (dishId,rating,author,comment) => ({
+export const addComment = (dishId, rating, author, comment) => ({
   type: ActionTypes.ADD_COMMENT,
-  payload:{
+  payload: {
     dishId: dishId,
     rating: rating,
     author: author,
@@ -71,20 +100,35 @@ export const fetchPromos = () => (dispatch) => {
   dispatch(promosLoading(true));
 
   return fetch(baseUrl + 'promotions')
-  .then(response=> response.json())
-  .then(promotions => dispatch(addPromos(promotions))) 
+    .then(response => {
+      if (response.ok) {
+        return response;
+      }
+      else {
+        var error = new Error('Error' + response.status + ":" + response.statusText)
+        error.response = response;
+        throw error;
+      }
+    },
+      error => {
+        var errmess = new Error(error.message)
+        throw errmess;
+      })
+    .then(response => response.json())
+    .then(promotions => dispatch(addPromos(promotions)))
+    .catch(error => dispatch(promosFailed(error.message)))
 }
 
 export const addPromos = (promotions) => ({
-  type:ActionTypes.ADD_PROMOS,
-  payload:promotions
+  type: ActionTypes.ADD_PROMOS,
+  payload: promotions
 })
 
 export const promosFailed = (errmess) => ({
-  type:ActionTypes.PROMOS_FAILED,
-  payload:errmess
+  type: ActionTypes.PROMOS_FAILED,
+  payload: errmess
 })
 
 export const promosLoading = () => ({
-  type:ActionTypes.PROMOS_LOADING
+  type: ActionTypes.PROMOS_LOADING
 })
