4968c608cb7b23548534ca0c659f28412aef0854
Sanishmdhr <sanish.manandhar20@gmail.com>
2020-09-14 12:33:00 +0545

    Fetch Post Comment

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -12,9 +12,9 @@ export const Comments = (state = {
     case ActionTypes.COMMENTS_FAILED:
       return {...state, errMess:action.payload, comments:[]}  
 
-    // case ActionTypes.ADD_COMMENT:
-    //   var comment = action.payload;
-    //   return {...state, comments: state.comments.concat(comment)}
+    case ActionTypes.ADD_COMMENT:
+      var comment = action.payload;
+      return {...state, comments: state.comments.concat(comment)}
 
     default:
       return state;
